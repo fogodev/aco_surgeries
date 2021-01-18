28e36262a9d89c768c1e8d8973094b34d1f075a0
Tiago Montalvão <tiago.montalvao@olxbr.com>
2021-01-18 10:11:48 -0300

    Update script to generate ttt plots

--- scripts/plot_ttt.py
+++ scripts/plot_ttt.py
@@ -2,25 +2,35 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-for parallel in (False,): #(True, False):
-    for instance in range(1, 2): # range(1, 15):
-        par = str(parallel).lower()
-        infile = f'sample_data/"Indefinidas - i{instance}_durations_par_{par}.dat"'
-        outfile = f'sample_data/tttplot-out/{instance}_{par}'
-        tttfile = outfile + '-ee.dat'
-        plotfile = f'sample_data/plots/{instance}_{par}.png'
+instance = 9
+ants_threads = [(16, 16), (16, 1)]
+legends = ["16 threads / 16 formigas / com elitismo", "1 thread / 16 formigas / sem elitismo"]
+colors = ['#1f77b4', '#ff7f0e']
 
-        print(infile)
-        print(outfile)
-        cmd = f'perl scripts/tttplots.pl -f {infile} -o {outfile}'
-        print(cmd)
-        os.system(cmd)
+fig, ax = plt.subplots()
+plotfile = f'sample_data/plots/i{instance}'
 
-        data = pd.read_csv(tttfile, delim_whitespace=True, header=None)
+for legend, color, (ants, threads) in zip(legends, colors, ants_threads):
+    infile = f'sample_data/"Indefinidas - i{instance}_durations_{ants}_ants_{threads}_threads.dat"'
+    outfile = f'sample_data/tttplot-out/i{instance}_{ants}_ants_{threads}_threads'
+    tttfile = outfile + '-ee.dat'
 
-        print(data)
-        print(data[0], data[1])
+    print(infile)
+    print(outfile)
+    cmd = f'perl scripts/tttplots.pl -f {infile} -o {outfile}'
+    print(cmd)
+    os.system(cmd)
 
-        data.plot(x=0, y=1)
-        plt.savefig(plotfile)
-        plt.show()
+    data = pd.read_csv(tttfile, delim_whitespace=True, header=None)
+    data.columns = ["Tempo (s)", "Probabilidade (p)"]
+
+    print(data)
+    print(data["Tempo (s)"], data["Probabilidade (p)"])
+
+    data.plot(x="Tempo (s)", y="Probabilidade (p)", lw=0.3, ax=ax, label='', color=color)
+    data.plot.scatter(x="Tempo (s)", y="Probabilidade (p)", ax=ax, label=legend, color=color)
+
+plt.title(f'Instância {instance}')
+plt.legend(loc='lower right')
+plt.savefig(plotfile)
+plt.show()
